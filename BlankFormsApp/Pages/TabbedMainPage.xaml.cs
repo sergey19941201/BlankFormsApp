feda2353b8d7ff14118371add1ead1d7a3320514
Serhii <Koshelnyk>
2020-04-08 16:33:21 +0300

    Properties

--- BlankFormsApp/Pages/TabbedMainPage.xaml.cs
+++ BlankFormsApp/Pages/TabbedMainPage.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,6 +14,23 @@ namespace BlankFormsApp.Pages
         public TabbedMainPage()
         {
             InitializeComponent();
+
+            AddProperties();
+        }
+
+        private async void AddProperties()
+        {
+            object name;
+            if(App.Current.Properties.TryGetValue("name", out name))
+            {
+                // выполняем действия, если в словаре есть ключ "name"
+            }
+            else
+            {
+                name = "Tom";
+                App.Current.Properties.Add("name", name);
+                await App.Current.SavePropertiesAsync();
+            }
         }
     }
 }
