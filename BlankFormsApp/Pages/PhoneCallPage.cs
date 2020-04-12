a414a9a2f19220454007c5be604d569d345e2254
Serhii <Koshelnyk>
2020-04-12 12:53:01 +0300

    Phone Call Update, Messaging, Email

--- BlankFormsApp/Pages/PhoneCallPage.cs
+++ BlankFormsApp/Pages/PhoneCallPage.cs
@@ -1,4 +1,5 @@
 using BlankFormsApp.Interfaces;
+using Plugin.Messaging;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
@@ -7,20 +8,42 @@ namespace BlankFormsApp.Pages
     public class PhoneCallPage : ContentPage
     {
         Entry phoneNumberEntry;
-        Button callButton;
+        Button callButton, messageButton, emailButton;
 
         public PhoneCallPage()
         {
             this.Padding = new Thickness(10, 80, 10, 10);
 
-            Title = "Телефон";
             phoneNumberEntry = new Entry {Placeholder = "Введите номер"};
             callButton = new Button {Text = "Call"};
-            callButton.Clicked += async (o, e) => { PhoneDialer.Open(phoneNumberEntry.Text); };
+            callButton.Clicked += (o, e) =>
+            {
+                var phoneDialer = CrossMessaging.Current.PhoneDialer;
+                if (phoneDialer.CanMakePhoneCall)
+                    phoneDialer.MakePhoneCall(phoneNumberEntry.Text);
+            };
+
+            messageButton = new Button {Text = "Message"};
+            messageButton.Clicked += (o, e) =>
+            {
+                var smsMessenger = CrossMessaging.Current.SmsMessenger;
+                if (smsMessenger.CanSendSms)
+                    smsMessenger.SendSms("+79876543210", "Hello World!");
+            };
+
+            emailButton = new Button {Text = "Email"};
+            emailButton.Clicked += (o, e) =>
+            {
+                var emailMessenger = CrossMessaging.Current.EmailMessenger;
+                if (emailMessenger.CanSendEmail)
+                {
+                    emailMessenger.SendEmail("адрес получателя", "тема письма", "текст письма");
+                } 
+            };
 
             Content = new StackLayout
             {
-                Children = {phoneNumberEntry, callButton}
+                Children = {phoneNumberEntry, callButton, messageButton, emailButton}
             };
         }
     }
