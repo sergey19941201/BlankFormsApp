05c6485c6a5b4fbf3a342875460c61e25c98e4ea
Serhii <Koshelnyk>
2020-04-16 17:14:58 +0300

    CollectionView with Custom DataTemplate

---
+++ BlankFormsApp/Cells/CustomCollectionViewDataTemplate.cs
@@ -0,0 +1,30 @@
+using Xamarin.Forms;
+using System.Reflection;
+
+namespace BlankFormsApp.Cells
+{
+    public class CustomCollectionViewDataTemplate : Grid
+    {
+        public  CustomCollectionViewDataTemplate ()
+        {
+            Padding = 10;
+            RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+            RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
+            ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+            ColumnDefinitions.Add(new ColumnDefinition { Width = GridLength.Auto });
+
+            Image image = new Image { Aspect = Aspect.AspectFill, HeightRequest = 60, WidthRequest = 60 };
+            image.SetBinding(Image.SourceProperty, "ImagePath");
+
+            var nameLabel = new Label { FontAttributes = FontAttributes.Bold };
+            nameLabel.TextColor = Color.Brown;
+            nameLabel.SetBinding(Label.TextProperty, "Title");
+
+            Grid.SetRowSpan(image, 2);
+
+            Children.Add(image);
+            Children.Add(nameLabel, 1, 0);
+            BackgroundColor=Color.Aqua;
+        }
+    }
+}
--- BlankFormsApp/Pages/CollectionViewPage.xaml.cs
+++ BlankFormsApp/Pages/CollectionViewPage.xaml.cs
@@ -5,6 +5,8 @@ using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BlankFormsApp.Cells;
+using BlankFormsApp.Entities;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,7 +15,7 @@ namespace BlankFormsApp.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CollectionViewPage : ContentPage
     {
-        public ObservableCollection<string> CollectionSource { get; set; }
+        public ObservableCollection<Phone> CollectionSource { get; set; }
 
         public CollectionViewPage()
         {
@@ -28,32 +30,44 @@ namespace BlankFormsApp.Pages
 
             // IMPORTANT!!! HERE AFTER AWAIT
             this.BindingContext = this;
-            
+
+            // HACK To properly update auto-height item depending on text height ios
+            CollectionSource.Add(null);
+            CollectionSource.RemoveAt(CollectionSource.Count - 1);
+
             await Task.Delay(1000);
+
             AddSomeItems();
         }
 
-        private async Task<ObservableCollection<string>> GetItemsFromService()
+        private async Task<ObservableCollection<Phone>> GetItemsFromService()
         {
             await Task.Delay(2000);
-            
-            return new ObservableCollection<string>
+
+            return new ObservableCollection<Phone>
             {
-                "gjgjjg",
-                "fddf",
-                "Bdffddf",
-                "Squdffdy",
-                "Godfin",
-                "Hfd",
-                "Japdf"
+                new Phone
+                {
+                    Title =
+                        "Galaxy S8Galaxy S8Galaxy S8Galaxy S8Galaxy S8Galaxy S8Galaxy S8Galaxy S8Galaxy S8Galaxy S8Galaxy S8",
+                    ImagePath = "https://i.ytimg.com/vi/Ekb_I21Gssc/maxresdefault.jpg"
+                },
+                new Phone {Title = "Huawei P10", ImagePath = "gorilla.png"},
+                new Phone {Title = "HTC U Ultra", ImagePath = "monkey_circle.png"},
+                new Phone {Title = "iPhone 7", ImagePath = "gorilla.png"}
             };
         }
 
         private void AddSomeItems()
         {
-            CollectionSource.Add("gjjgjggggggggggggggg");
-            CollectionSource.Add("gjjgjggggggggggggggg");
-            CollectionSource.Add("gjjgjggggggggggggggg");
+            for (int i = 0; i < 100; i++)
+            {
+                CollectionSource.Add(new Phone
+                {
+                    Title = i.ToString(),
+                    ImagePath = "https://i.ytimg.com/vi/Ekb_I21Gssc/maxresdefault.jpg"
+                });
+            }
         }
     }
 }
